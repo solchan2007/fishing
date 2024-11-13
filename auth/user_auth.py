6eeb818bc8e32fd4dfde4757a4b01349821a78a1
solchan2007 <thfcksbubble@gmail.com>
2024-11-13 21:56:24 +0900

    회원가입시 없는 아이디 비번 불가능하게 설정

--- auth/user_auth.py
+++ auth/user_auth.py
@@ -20,56 +20,59 @@ def register(register_info: dict):
 		data_dict = json_file_to_dict()
 		username = register_info['username']
 		password = register_info['password']
-		if(username in data_dict):
-			result.update({"success": False, "errormessage": '이미 존재하는 아이디입니다.'})
-		else:
-			data_dict[username] = {
-				"username": username,
-				"password": password,
-				"price": 50000,
-				"inventory": {
-					"items": [],
-					"fishing_rods": [
-						{
-							"name": "초보자 낚싯대",
-							"durability": 400,
-							"equipped": 1,
-							"grade": "shallow"
-						}
-					]
-				},
-				"hunger": 100,
-				"current_depth_level": "shallow",
-				"game_progress": {
-					"date": 0,
-					"time_of_day": "morning"
-				},
-				"ranking": {
-					"rank": 0,
-					"earnings": 0
+		if(username != '' and password != ''):
+			if(username in data_dict):
+				result.update({"success": False, "errormessage": '이미 존재하는 아이디입니다.'})
+			else:
+				data_dict[username] = {
+					"username": username,
+					"password": password,
+					"price": 50000,
+					"inventory": {
+						"items": [],
+						"fishing_rods": [
+							{
+								"name": "초보자 낚싯대",
+								"durability": 400,
+								"equipped": 1,
+								"grade": "shallow"
+							}
+						]
+					},
+					"hunger": 100,
+					"current_depth_level": "shallow",
+					"game_progress": {
+						"date": 0,
+						"time_of_day": "morning"
+					},
+					"ranking": {
+						"rank": 0,
+						"earnings": 0
+					}
 				}
-			}
-			# 음식 정보 불러오기
-			file1 = open("fooddata.json", "r", encoding='UTF-8')
-			jsondata1 = json.load(file1)
-			file1.close()
-			# 물고기 정보 불러오기
-			file2 = open("fishdata.json", "r", encoding='UTF-8')
-			jsondata2 = json.load(file2)
-			file2.close()
-			# 낚싯대 정보 불러오기
-			file3 = open("fishingroddata.json", "r", encoding='UTF-8')
-			jsondata3 = json.load(file3)
-			file3.close()
+				# 음식 정보 불러오기
+				file1 = open("fooddata.json", "r", encoding='UTF-8')
+				jsondata1 = json.load(file1)
+				file1.close()
+				# 물고기 정보 불러오기
+				file2 = open("fishdata.json", "r", encoding='UTF-8')
+				jsondata2 = json.load(file2)
+				file2.close()
+				# 낚싯대 정보 불러오기
+				file3 = open("fishingroddata.json", "r", encoding='UTF-8')
+				jsondata3 = json.load(file3)
+				file3.close()
 
-			for food in jsondata1['foods']:
-				data_dict[username]['inventory']['items'].append({"name": food['name'], "type": "food", "desc": food['desc'], "quantity": 0})
-			for fish in jsondata2['fishes']:
-				if(fish['available']):
-					data_dict[username]['inventory']['items'].append({"name": fish['name'], "type": "fish", "desc": fish['desc'], "quantity": 0})
-			for fishingrod in jsondata3['fishing_rods']:
-				data_dict[username]['inventory']['fishing_rods'].append({"name": fishingrod['name'], "durability": 0, "equipped": 0, "grade": fishingrod['grade']})
-			result.update({"success": True, "errormessage": ""})
+				for food in jsondata1['foods']:
+					data_dict[username]['inventory']['items'].append({"name": food['name'], "type": "food", "desc": food['desc'], "quantity": 0})
+				for fish in jsondata2['fishes']:
+					if(fish['available']):
+						data_dict[username]['inventory']['items'].append({"name": fish['name'], "type": "fish", "desc": fish['desc'], "quantity": 0})
+				for fishingrod in jsondata3['fishing_rods']:
+					data_dict[username]['inventory']['fishing_rods'].append({"name": fishingrod['name'], "durability": 0, "equipped": 0, "grade": fishingrod['grade']})
+				result.update({"success": True, "errormessage": ""})
+		else:
+			result.update({"success": False, "errormessage": "username 또는 password 값이 누락되었습니다."})
 		dict_to_json_file(data_dict)
 	else:
 		result.update({"success": False, "errormessage": "username 또는 password 값이 누락되었습니다."})
